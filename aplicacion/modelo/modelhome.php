5a80e0ade332b3254d47561329b482f6f80b5c87
carlochess <carlochess@hotmail.com>
2014-05-19 23:52:11 -0500

    Probando Propel

--- aplicacion/modelo/modelhome.php
+++ aplicacion/modelo/modelhome.php
@@ -1,26 +1,43 @@
 
 <?php
 
+// configurar autoloading
+require_once '../libs/vendor/autoload.php';
+
+// configurar Propel
+require_once '../libs/generated-conf/config.php';
+
 class ModelHome
 {
 	/* Clase encargada de las consultas a la bd*/
 	private $oMySQL;
 	
-	function __construct(MySQL $db)
+	function __construct(/*MySQL*/ $db)
 	{
-		
-		$this->oMySQL = $db;
+            $this->oMySQL = $db;
 	}
 	/** Retorna las categorías que agrupan a los producto */
 	function /* array(stdObject) */ getCategorias()
 	{
-		return $this->oMySQL->ejecutarConsultaSelect('SELECT DISTINCT categoria FROM producto WHERE 1');
+            $consulta = \Base\ProductoQuery::create()->select("*")->distinct()->find();
+            return $consulta->toArray();
+            //return $this->oMySQL->ejecutarConsultaSelect('SELECT DISTINCT categoria FROM producto');
 	}
 	
 	/**Retorna los productos mas vendidos */
 	function getProdMasVend()
 	{
-		return $this->oMySQL->ejecutarConsultaSelect('SELECT id_prod, COUNT(cant_prod) FROM compra ORDER BY cant_prod DESC LIMIT 6');
+            $consulta = \Base\ProductoQuery::create()
+                    ->select("*")
+                    ->addAsColumn("cantidad", 'COUNT(unidades)')
+                    ->groupBy("unidades")//'SUM(price_equipment)'
+                    ->orderBy("cantidad", "DESC")
+                    ->limit(6)
+                    ->find();
+            return $consulta->toArray();
+            //return $this->oMySQL->ejecutarConsultaSelect('SELECT id_prod, COUNT(cant_prod) FROM compra ORDER BY cant_prod DESC LIMIT 6');
+            //                                            SELECT COUNT(unidades) AS cantidad FROM  GROUP BY producto.UNIDADES ORDER BY cantidad ASC LIMIT 6
+
 	}
 	
 	/** Retorna las ofertas de día */
@@ -39,13 +56,27 @@ now() between precio.fecha_ini and precio.fecha_fin";
 	/** Retorna N objetos aleatorios */
 	function getProdAleatorios()
 	{
-		return $this->oMySQL->ejecutarConsultaSelect('SELECT producto.id_prod AS id_prod, nombre, descripcion,empresa_fab,iva, producto.id_prod AS ruta,precio.valor AS precio  FROM producto,precio WHERE producto.id_prod=precio.cod_producto ORDER BY RAND() LIMIT 3 ');
+            $consulta = \Base\ProductoQuery::create()
+                    ->joinPrecio()
+                    ->select("*")
+                    ->find();
+            return $consulta->toArray();
+            //return $this->oMySQL->ejecutarConsultaSelect('SELECT producto.id_prod AS id_prod, nombre, descripcion,empresa_fab,iva, producto.id_prod AS ruta,precio.valor AS precio  FROM producto,precio WHERE producto.id_prod=precio.cod_producto ORDER BY RAND() LIMIT 3 ');
 	}
 	
 	/** Retorna los 10 productos mas vendidos*/
 	function getMasVendidos()
 	{
-		return $this->oMySQL->ejecutarConsultaSelect('SELECT id_prod, count(id_prod) AS veces ,count(cant_prod) as cantidad FROM compra GROUP BY id_prod ORDER BY veces DESC LIMIT 10 ');
+            $consulta = \Base\ProductoQuery::create()
+                    ->select("*")
+                    ->addAsColumn("veces", 'count(id_prod)')
+                    ->addAsColumn("cantidad", 'count(unidades)')
+                    ->groupBy("id_prod")//'SUM(price_equipment)'
+                    ->orderBy("veces", "DESC")
+                    ->limit(10)
+                    ->find();
+            return $consulta->toArray();
+            //return $this->oMySQL->ejecutarConsultaSelect('SELECT id_prod, count(id_prod) AS veces ,count(cant_prod) as cantidad FROM compra GROUP BY id_prod ORDER BY veces DESC LIMIT 10 ');
 	}
 	
 	function terminarConexion()
@@ -53,4 +84,13 @@ now() between precio.fecha_ini and precio.fecha_fin";
 		$this->oMySQL->cerrarConexion();
 	}
 }
+
+function main()
+{
+    $objHome = new ModelHome(NULL);
+    print_r($objHome->getProdAleatorios());
+}
+
+main();
+
 ?>
--- index.php
+++ index.php
@@ -1,44 +1,22 @@
 <?php
 date_default_timezone_set('America/Bogota');
 // Carga la configuración
-//require 'aplicacion/cfg/config.php';
+require 'aplicacion/cfg/config.php';
 
 // Carga el controlador de la aplicación (CGI)
-//require 'aplicacion/libs/uvshop.php';
+require 'aplicacion/libs/uvshop.php';
 
 //require 'aplicacion/libs/login.php';
 
 // Carga el controlador 
-//require 'aplicacion/libs/controlador.php';
+require 'aplicacion/libs/controlador.php';
 /*
 if (file_exists('vendor/autoload.php')) {
     require 'vendor/autoload.php';
 }*/
 
-// configurar autoloading
-require_once 'aplicacion/libs/vendor/autoload.php';
-
-// configurar Propel
-require_once 'aplicacion/libs/generated-conf/config.php';
-
-$producto = new Producto();
-$producto->setIdProd('Adios');
-$producto->setNombre('Cristian'); 
-$producto->setEmpresaFab('Hola');
-$producto->setDescripcion('Ninguna');
-$producto->setIva(20);
-$producto->setCategoria('Idiotas');
-$producto->setUnidades(1);
-try{
-    $producto->save();
-}  catch (PDOException $ex){
-    echo 'Error al insertar';
-}
-catch (Exception $ex) {
-    echo 'Error al insertar';
-}
 
 
 // iniciar la tienda virtual
-//$app = new Uvshop();
+$app = new Uvshop();
 ?>
